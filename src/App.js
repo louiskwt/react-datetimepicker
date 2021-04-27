626f02a5a66edb5fefb9119437a0d18bf8bdf2dd
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-27 19:31:57 +0800

    date picker set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+	MuiPickersUtilsProvider,
+	KeyboardTimePicker,
+	KeyboardDatePicker
+} from '@material-ui/pickers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selectedDate, setSelectedDate] = useState(
+		new Date('2021-04-27T12:00:00')
+	);
+
+	const handleDateChange = (date) => {
+		setSelectedDate(date);
+	};
+
+	return (
+		<div className='App'>
+			<MuiPickersUtilsProvider utils={DateFnsUtils}>
+				<Grid container justify='space-around'>
+					<KeyboardDatePicker
+						disableToolbar
+						variant='inline'
+						format='MM/dd/yyy'
+						margin='normal'
+						id='date-picker'
+						label='Date Picker'
+						value={selectedDate}
+						onChange={handleDateChange}
+						KeyboardButtonProps={{
+							'aria-label': 'change date'
+						}}
+					/>
+				</Grid>
+			</MuiPickersUtilsProvider>
+		</div>
+	);
 }
 
 export default App;
